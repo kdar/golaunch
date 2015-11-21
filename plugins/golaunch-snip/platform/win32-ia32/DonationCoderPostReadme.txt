225c2d775df94e0dddcbeec3f6e5a7331e55f657
Kevin Darlington <kevin@outroot.com>
2015-11-21 15:02:29 -0500

    added snip plugin

---
+++ plugins/golaunch-snip/platform/win32-ia32/DonationCoderPostReadme.txt
@@ -0,0 +1,8 @@
+----------------------------------------------------
+DonationCoder.com
+supported by user contributions
+----------------------------------------------------
+
+Visit http://www.donationcoder.com for our power-user forum,
+ more exclusive freeware programs, software reviews, daily columns,
+ discounts on award winning shareware, and more.
---
+++ plugins/golaunch-snip/snip.js
@@ -0,0 +1,47 @@
+// Minicap -captureregselect -clipimage -exit
+
+var electron = require('electron');
+var app = electron.app;
+var events = require('events');
+var spawn = require('child_process').spawn;
+var os = require('os');
+var path = require('path');
+
+var Plugin = function() {
+  var self = this;
+
+  this.on('request', function(data) {
+    if (data.method == "init") {
+      self.metadata = data.params;
+    } else if (data.method == "query") {
+      if (data.params[0].startsWith("snip")) {
+        self.emit('response', {
+          'result': [{
+            image: self.metadata._icon,
+            title: "Screen Snipping",
+            subtitle: "Snip screen to clipboard",
+            score: -1,
+            query: data.params[0],
+            id: self.metadata.id
+          }]
+        });
+      }
+    } else if (data.method == "action") {
+      var platform = os.platform();
+      var arch = os.arch();
+      switch (platform + arch) {
+        case "win32ia32":
+        case "win32x64":
+          var child = spawn(path.join(__dirname, "platform", "win32-ia32", "MiniCap.exe"), ['-captureregselect', '-clipimage', '-exit'], {
+            detached: true
+          });
+          child.unref();
+        break;
+      }
+    }
+  });
+};
+
+Plugin.prototype.__proto__ = events.EventEmitter.prototype;
+
+module.exports = Plugin;
