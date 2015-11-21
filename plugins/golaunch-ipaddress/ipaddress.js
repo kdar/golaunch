cfe02a67f3d08a1e31c147a4573b4b112fcd93a4
Kevin Darlington <kevin@outroot.com>
2015-11-21 14:11:35 -0500

    added ipaddress plugin

---
+++ plugins/golaunch-ipaddress/ipaddress.js
@@ -0,0 +1,55 @@
+var electron = require('electron');
+var app = electron.app;
+var events = require('events');
+var clipboard = require('clipboard');
+var locip = require('ip');
+var extip = require('external-ip')();
+
+var Plugin = function() {
+  var self = this;
+
+  this.on('request', function(data) {
+    if (data.method == "init") {
+      self.metadata = data.params;
+    } else if (data.method == "query") {
+      if (data.params[0].startsWith("ipaddress")) {
+        extip(function (err, eip) {
+          var results = [];
+
+          if (!err) {
+            results.push({
+              image: self.metadata._icon,
+              title: "External IP: " + eip,
+              subtitle: "Copy to clipboard",
+              score: -1,
+              query: data.params[0],
+              id: self.metadata.id,
+              data: eip
+            });
+          }
+
+          var localip = locip.address();
+          results.push({
+            image: self.metadata._icon,
+            title: "Local IP: " + localip,
+            subtitle: "Copy to clipboard",
+            score: -1,
+            query: data.params[0],
+            id: self.metadata.id,
+            data: localip
+          });
+
+          self.emit('response', {
+            'result': results
+          });
+        });
+      }
+    } else if (data.method == "action") {
+      clipboard.writeText(data.params.data);
+    }
+  });
+};
+
+Plugin.prototype.__proto__ = events.EventEmitter.prototype;
+
+module.exports = Plugin;
