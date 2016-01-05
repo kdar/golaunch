a667fb5ce7fc10d9918cd8bc487aa4c6fb739888
Kevin Darlington <kevin@outroot.com>
2016-01-04 22:54:39 -0500

    added plugin helper for go in sdk

---
+++ sdk/go/plugin/plugin.go
@@ -0,0 +1,69 @@
+package plugin
+
+import (
+	"encoding/json"
+	"golaunch/sdk/go"
+	"os"
+)
+
+type Plugin interface {
+	Init(sdk.Metadata)
+	Query(string)
+	Action(sdk.Action)
+}
+
+type Client struct {
+	enc *json.Encoder
+}
+
+func (c *Client) QueryResults(results []sdk.QueryResult) {
+	msg := sdk.Response{
+		Result: results,
+	}
+	c.enc.Encode(msg)
+}
+
+func NewClient() *Client {
+	return &Client{
+		enc: json.NewEncoder(os.Stdout),
+	}
+}
+
+type Server struct {
+	dec *json.Decoder
+	p   Plugin
+}
+
+func NewServer() *Server {
+	return &Server{
+		dec: json.NewDecoder(os.Stdin),
+	}
+}
+
+func (s *Server) Register(p Plugin) {
+	s.p = p
+}
+
+func (s *Server) Serve() {
+	for {
+		var v sdk.Request
+		if err := s.dec.Decode(&v); err != nil {
+			continue
+		}
+
+		switch v.Method {
+		case "init":
+			var metadata sdk.Metadata
+			json.Unmarshal(v.Params, &metadata)
+			s.p.Init(metadata)
+		case "query":
+			var query string
+			json.Unmarshal(v.Params, &query)
+			s.p.Query(query)
+		case "action":
+			var action sdk.Action
+			json.Unmarshal(v.Params, &action)
+			s.p.Action(action)
+		}
+	}
+}
