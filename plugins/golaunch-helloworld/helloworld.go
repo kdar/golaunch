8e87b304b93c872eee5a3ce0483164937186d86f
Kevin Darlington <kevin@outroot.com>
2016-01-05 21:15:42 -0500

    added example helloworld plugin

---
+++ plugins/golaunch-helloworld/helloworld.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	sdk "golaunch/sdk/go"
+	"golaunch/sdk/go/plugin"
+	"log"
+	"os"
+)
+
+type Plugin struct {
+	metadata sdk.Metadata
+	client   *plugin.Client
+}
+
+func NewPlugin() *Plugin {
+	return &Plugin{
+		client: plugin.NewClient(),
+	}
+}
+
+func (p *Plugin) Init(m sdk.Metadata) {
+	p.metadata = m
+}
+
+func (p *Plugin) Query(q string) {
+	if q == "helloworld" {
+		p.client.QueryResults([]sdk.QueryResult{{
+			Title:    "Hello back to you!",
+			Subtitle: "From the helloworld plugin",
+			Query:    q,
+			ID:       p.metadata.ID,
+			Score:    -1,
+		}})
+	}
+}
+
+func (p *Plugin) Action(a sdk.Action) {
+	log.Print("action for helloworld pressed")
+}
+
+func main() {
+	log.SetFlags(log.Lshortfile)
+	log.SetOutput(os.Stderr)
+
+	s := plugin.NewServer()
+	s.Register(NewPlugin())
+	s.Serve()
+}
