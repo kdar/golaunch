7c309b00ffb6319414997716b306dda992cf890c
Kevin Darlington <kevin@outroot.com>
2016-01-06 17:36:01 -0500

    forgot to updated sys plugin to new api

--- plugins/golaunch-sys/sys.js
+++ plugins/golaunch-sys/sys.js
@@ -9,7 +9,7 @@ var path = require('path');
 var Plugin = function() {
   var self = this;
 
-  var commands = {
+  self.commands = {
     "restart": {
       title: "Restart GoLaunch",
       subtitle: "Restarts the application immediately."
@@ -25,61 +25,65 @@ var Plugin = function() {
   };
 
   sdk.imageFileToEmbed(path.join(__dirname, "images", "exit.png")).then(function(img) {
-    commands["quit"].icon = img;
-    commands["exit"].icon = img;
+    self.commands["quit"].icon = img;
+    self.commands["exit"].icon = img;
   }, function(err) {
     console.error(err);
   });
 
   sdk.imageFileToEmbed(path.join(__dirname, "images", "restart.png")).then(function(img) {
-    commands["restart"].icon = img;
+    self.commands["restart"].icon = img;
   }, function(err) {
     console.error(err);
-  });
-
-  this.on('request', function(data) {
-    if (data.method == "init") {
-      self.metadata = data.params;
-    } else if (data.method == "query") {
-      var results = [];
-      for (var key in commands) {
-        var match = fuzzy.match(data.params, key);
-        if (match.success) {
-          results.push({
-            icon: commands[key].icon,
-            title: commands[key].title,
-            subtitle: commands[key].subtitle,
-            score: match.score,
-            query: data.params,
-            id: self.metadata.id,
-            data: key
-          });
-        }
-      }
-
-      if (results.length > 0) {
-        self.emit('response', {
-          'result': results
-        });
-      }
-    } else if (data.method == "action") {
-      if (data.params.queryResult.data == "restart") {
-        const args = process.argv.slice(1);
-        //const out = fs.openSync('./out.log', 'a');
-        //const err = fs.openSync('./out.log', 'a');
-        var child = spawn(process.argv[0], args, {
-          detached: true,
-          //stdio: [ 'ignore', out, err ]
-        });
-        child.unref();
-        app.quit();
-      } else if (data.params.queryResult.data == "quit" || data.params.queryResult.data == "exit") {
-        app.quit();
-      }
-    }
   });
 };
 
 Plugin.prototype.__proto__ = events.EventEmitter.prototype;
+
+Plugin.prototype.init = function init(metadata) {
+  this.metadata = metadata;
+};
+
+Plugin.prototype.query = function query(query) {
+  var self = this;
+
+  var results = [];
+  for (var key in self.commands) {
+    var match = fuzzy.match(query, key);
+    if (match.success) {
+      results.push({
+        icon: self.commands[key].icon,
+        title: self.commands[key].title,
+        subtitle: self.commands[key].subtitle,
+        score: match.score,
+        query: query,
+        id: self.metadata.id,
+        data: key
+      });
+    }
+  }
+
+  if (results.length > 0) {
+    self.emit('response', {
+      'result': results
+    });
+  }
+};
+
+Plugin.prototype.action = function action(action) {
+  if (action.queryResult.data == "restart") {
+    const args = process.argv.slice(1);
+    //const out = fs.openSync('./out.log', 'a');
+    //const err = fs.openSync('./out.log', 'a');
+    var child = spawn(process.argv[0], args, {
+      detached: true,
+      //stdio: [ 'ignore', out, err ]
+    });
+    child.unref();
+    app.quit();
+  } else if (action.queryResult.data == "quit" || action.queryResult.data == "exit") {
+    app.quit();
+  }
+};
 
 module.exports = Plugin;
