7185d1c1607fa2db1ffdab7da2b540f99e84cdf8
Kevin Darlington <kevin@outroot.com>
2016-01-08 15:08:43 -0500

    removed external-ip package since it used the slow 'request' package
    use my own code now

--- plugins/golaunch-ipaddress/ipaddress.js
+++ plugins/golaunch-ipaddress/ipaddress.js
@@ -3,7 +3,59 @@ var app = electron.app;
 var events = require('events');
 var clipboard = require('clipboard');
 var locip = require('ip');
-var extip = require('external-ip')();
+var http = require('http');
+
+var extip = function(cb) {
+  var services = [
+    'http://curlmyip.com/',
+    'http://icanhazip.com/',
+    'http://myexternalip.com/raw'
+  ];
+
+  var done = false;
+  var errors = [];
+  var requests;
+
+  var abort = function(requests) {
+    process.nextTick(function() {
+      requests.forEach(function(request) {
+        request.abort();
+      });
+    });
+  };
+
+  var onResponse = function(err, ip) {
+    if (done) {
+      return;
+    }
+    if (err) {
+      errors.push(err);
+    }
+    if (ip) {
+      done = true;
+      abort(requests); //async
+      return cb(null, ip);
+    }
+    if (errors.length === services.length) {
+      done = true;
+      abort(requests); //async
+      return cb(errors, null);
+    }
+  };
+
+  requests = services.map(function(service) {
+    var req = http.request(service, function(res) {
+      res.on('data', function(data) {
+        onResponse(null, data);
+      });
+    });
+    req.on('error', function(e) {
+      onResponse(e.message, null);
+    });
+    req.end();
+    return req;
+  });
+};
 
 var Plugin = function() {};
 
@@ -17,7 +69,7 @@ Plugin.prototype.query = function query(query) {
   var self = this;
 
   if (query.startsWith("ipaddress")) {
-    extip(function (err, eip) {
+    extip(function(err, eip) {
       var results = [];
 
       if (!err) {
